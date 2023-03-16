cf85b9a1587848a52c53f4c3cea5439d13660ada
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-16 13:39:27 +0800

    Add: form to add inventory

--- resources/views/Inventory/create.blade.php
+++ resources/views/Inventory/create.blade.php
@@ -1,1 +1,31 @@
-create
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add New Inventory</div>
+
+                <div class="card-body">
+                <form method="POST" action="{{route ('inventory.store')}}">
+                        @csrf
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nama Inventori</label>
+                        <input type="text" class="form-control" name="name">
+                        </div>
+                        <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Detail Inventori</label>
+                        <textarea class="form-control" rows="3" name="description"></textarea>
+
+                        <a href="{{route ('home')}}" type="button" class="btn btn-primary">Back</a>
+                        <button type="submit" class="btn btn-primary">Add</button>
+                    </div>
+                   
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
