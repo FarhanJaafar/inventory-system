65ab7f66de7c2594248155ccdf03740c1779a083
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-16 13:37:10 +0800

    add: function store

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
 use Illuminate\Http\Request;
 
 class InventoryController extends Controller
@@ -9,5 +10,16 @@ class InventoryController extends Controller
     public function create()
     {
         return view ("inventory.create");
+    }
+
+    public function store(Request $request)
+    {
+        Inventory::create([
+            'user_id' =>auth()->user()->id,
+            'name' =>$request->name,
+            'description'=>$request->description,
+        ]);
+
+        return to_route('home');
     }
 }
