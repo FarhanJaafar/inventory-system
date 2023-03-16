76e0bb728eebd94dafe5ba4506a5ac9b3e471ea0
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-16 13:56:36 +0800

    Add: return inventories data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventory;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $inventories = Inventory::all();
+        return view('home', compact ('inventories'));
     }
 }
