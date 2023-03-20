8e2d4ca599200bada33396f04ff19452f2a41a6a
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-20 11:04:25 +0800

    Add: route for search

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,10 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
 Route::get('/Inventory/create', [App\Http\Controllers\InventoryController::class, 'create'])->name('inventory.create');
 Route::post('/Inventory/store', [App\Http\Controllers\InventoryController::class, 'store'])->name('inventory.store');
 Route::get('/Inventory/edit/{inventory}', [App\Http\Controllers\InventoryController::class, 'edit'])->name('inventory.edit');
 Route::post('/Inventory/update/{inventory}', [App\Http\Controllers\InventoryController::class, 'update'])->name('inventory.update');
 Route::get('/Inventory/delete/{inventory}', [App\Http\Controllers\InventoryController::class, 'delete'])->name('inventory.delete');
+Route::get('/Inventory/search', [App\Http\Controllers\InventoryController::class, 'search'])->name('inventory.search');
