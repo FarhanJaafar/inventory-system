14617813bd13c9ca1f053465fdedd010af76dabe
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-21 10:42:10 +0800

    Create: Migration InventoryStock

---
+++ database/migrations/2023_03_20_031606_create_inventory_stocks.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_stocks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('inventory_id');
+            $table->foreign('inventory_id')->references('id')->on('inventories');
+            //$table->foreignId('inventory_id')->constrained('inventories');
+            $table->bigInteger('quantity');
+            $table->string('color');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_stocks');
+    }
+};
