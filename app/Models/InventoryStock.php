105c9d5c5725fe2b0db0af9563944420201a8950
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-21 10:38:13 +0800

    Create: InventoryStock model

---
+++ app/Models/InventoryStock.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Inventory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class InventoryStock extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'inventory_id',
+        'color',
+        'quantity',
+    ];
+}
