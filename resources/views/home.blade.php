4e6317d3e4c8b6140a8ce28cb67064e213b7a9a7
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-22 12:19:15 +0800

    Add: show button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,39 +6,37 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{auth::user()->name}}{{ __(' are logged in!') }}
-
-                    </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    {{auth::user()->name}}{{ __(' are logged in!') }}
                 </div>
-                <br>
-                <div class="card mt-2">
-              <div class="card-header">Type Inventory</div>
-              <div class="card-body">
-                <div class="input-group mt-2 p-2">
-                  <a href="{{ route('inventorytype.index') }}" type="button" class="btn btn-dark">Go to Inventory Type</a>
-                </div>
-              </div>
             </div>
             <br>
-                <div class="card">
-                    <div class="card-header">List Inventory </div>
-                    <form method="" action="">
-                        <div class="input-group mt-2 p-2">
-                            <input type="text" class="form-control"  name="keyword" value="{{ request()->get('keyword') }}" placeholder="Input Inventory Name">
-                            <div class="input-group-append">
-                                <button type="submit" class="btn btn-primary">Search</button>
-                            </div>  
-                        </div>
-                        <a href="{{route ('inventory.create')}}" type="button" class="btn btn-success">Add inventory</a>
-                    </form> 
-                    <table class="table">
+            <div class="card mt-2">
+                <div class="card-header">Type Inventory</div>
+                <div class="card-body">
+                    <div class="input-group mt-2 p-2">
+                        <a href="{{ route('inventorytype.index') }}" type="button" class="btn btn-dark">Go to Inventory Type</a>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header">List Inventory </div>
+                <form method="" action="">
+                    <div class="input-group mt-2 p-2">
+                        <input type="text" class="form-control"  name="keyword" value="{{ request()->get('keyword') }}" placeholder="Input Inventory Name">
+                        <div class="input-group-append">
+                            <button type="submit" class="btn btn-primary">Search</button>
+                        </div>  
+                    </div>
+                    <a href="{{route ('inventory.create')}}" type="button" class="btn btn-success">Add inventory</a>
+                </form> 
+                <table class="table">
                     <thead>
                         <tr>
                             <th scope="col">No</th>
@@ -55,15 +53,16 @@
                                 <td>{{$inventory->name}}</td>
                                 <td>{{$inventory->description}}</td>
                                 <td>{{$inventory->inventoryType->name}}</td>
-                                <td>
-                                    <a href="{{route ('inventory.edit', $inventory)}}" type="submit" name="edit" class="btn btn-warning">Edit</a>
-                                    <a href="{{route ('inventory.delete', $inventory)}}" type="button" class="btn btn-danger">Delete</a>
-                                </td>
+                            <td>
+                                <a href="{{route ('inventory.show', $inventory)}}" type="submit" name="edit" class="btn btn-warning">Show</a>   
+                                <a href="{{route ('inventory.edit', $inventory)}}" type="submit" name="edit" class="btn btn-warning">Edit</a>
+                                <a href="{{route ('inventory.delete', $inventory)}}" type="button" class="btn btn-danger">Delete</a>
+                            </td>   
                             </tr>
-                        @endforeach
+                        @endforeach 
                     </tbody>
-                    </table>
-                    {{$inventories->links()}}
+                </table>
+                {{$inventories->links()}}
                 </div>
             </div>
         </div>
