ec364c3ddf2abee4a6d946165acb2ab046c64dbf
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-22 12:20:54 +0800

    Add: show inventory type and add color/quantity form

--- resources/views/Inventory/edit.blade.php
+++ resources/views/Inventory/edit.blade.php
@@ -6,21 +6,28 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Edit Inventory</div>
-
                 <div class="card-body">
-                <form method="POST" action="{{route ('inventory.update', $inventory)}}">
+                    <form method="POST" action="{{route ('inventory.update', $inventory)}}">
                         @csrf
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Nama Inventori</label>
-                        <input type="text" class="form-control" name="name" placeholder="{{$inventory->name}}">
-                        </div>
                         <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label">Detail Inventori</label>
+                            <label for="exampleFormControlSelect1">Jenis Inventori</label>
+                            <select class="form-control" name="inventory_type_id">
+                                @foreach ( $inventoryTypes as $inventoryType )
+                                    <option value="{{ $inventoryType->id }}">{{ $inventoryType->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <label for="exampleFormControlInput1" class="form-label">Name Inventori</label>
+                        <input type="text" class="form-control" name="name" placeholder="{{$inventory->name}}">
+                        <label for="exampleFormControlTextarea1" class="form-label">Description Inventori</label>
                         <textarea class="form-control" rows="3" name="description" placeholder="{{$inventory->description}}"></textarea>
+                        <label for="exampleFormControlInput1" class="form-label">Color</label>
+                        <input type="text" class="form-control" name="color" placeholder="">
+                        <label for="exampleFormControlInput1" class="form-label">Quantity</label>
+                        <input type="text" class="form-control" name="quantity" placeholder="">
                         <a href="{{route ('home')}}" type="button" class="btn btn-primary">Back</a>
                         <button type="submit" class="btn btn-primary">Update</button>
-                    </div>
-                </form>
+                    </form>
                 </div>
             </div>
         </div>
