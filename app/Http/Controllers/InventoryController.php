ec0577e6544fe883991c2c09bf44cc9b7e8aceca
FarhanJaafar <farhanjaafar00@gmail.com>
2023-03-22 12:16:00 +0800

    Add: store for inventorystock and delete parent function

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Inventory;
 use Illuminate\Http\Request;
 use App\Models\InventoryType;
+use App\Models\InventoryStock;
 use App\Http\Requests\StoreInventoryRequest;
 
 class InventoryController extends Controller
@@ -17,11 +18,17 @@ class InventoryController extends Controller
 
     public function store(StoreInventoryRequest $request)
     {
-        Inventory::create([
+        $inventory = Inventory::create([
             'user_id' =>auth()->user()->id,
             'name' =>$request->name,
             'description'=>$request->description,
             'inventory_type_id'=>$request->inventory_type_id
+        ]);
+        //dd($inventory);
+        InventoryStock::create([
+            'inventory_id' =>$inventory->id,
+            'color' =>$request->color,
+            'quantity'=>$request->quantity
         ]);
 
         return to_route('home');
@@ -29,8 +36,8 @@ class InventoryController extends Controller
 
     public function edit(Inventory $inventory)
     {
-
-        return view('inventory.edit', compact('inventory'));
+        $inventoryTypes=InventoryType::all();
+        return view('inventory.edit', compact('inventory', 'inventoryTypes'));
     }
 
     public function update(Request $request, Inventory $inventory)
@@ -39,6 +46,14 @@ class InventoryController extends Controller
             'user_id' =>$inventory->user_id,
             'name' =>$request->name,
             'description'=>$request->description,
+            'inventory_type_id'=>$request->inventory_type_id
+        ]);
+
+        $stock = InventoryStock::where('inventory_id', $inventory->id);
+
+        $stock->update([
+            'color' =>$request->color,
+            'quantity'=>$request->quantity
         ]);
 
         return to_route('home');
@@ -46,8 +61,19 @@ class InventoryController extends Controller
 
     public function delete(Inventory $inventory)
     {
-        $inventory->delete([]);
+
+        $inventory->inventoryStock()->delete();
+        $inventory->delete();
 
         return to_route('home');
-    }  
+    }
+
+    public function show(Inventory $inventory)
+    {
+        $inventoryStocks = $inventory->inventoryStock;
+        return view('inventory.show', compact('inventory','inventoryStocks'));
+    }
+
+
+
 }
